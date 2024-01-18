a47767fe5961ef53478516730bac763f37052b1e
Ulamusadag <ulamusadag@gmail.com>
2024-01-18 06:40:09 -0800

    0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function use bubble_sort algorithm
+ * @array: pointer to array of intigers
+ * @size: size of the array
+ * Return: the sorted array
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0;
+	size_t j = 0;
+	int temp = 0;
+
+	for (i = 0; i < (size - 1); i++)
+	{
+		for (j = 0; j < (size - i - 1); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j + 1];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		
+		}
+	}
+
+}
